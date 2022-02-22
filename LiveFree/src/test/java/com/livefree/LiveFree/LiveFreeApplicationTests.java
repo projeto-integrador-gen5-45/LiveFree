eec40ccda3d9ab8571f82001e47a9f9dde3a76c5
Sam-S-Pina <lesmak.ss@gmail.com>
2022-02-22 11:29:36 -0300

    @Sam-Pina [TASK: CREATE Spring/Maven project LiveFree]

---
+++ LiveFree/src/test/java/com/livefree/LiveFree/LiveFreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livefree.LiveFree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveFreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
