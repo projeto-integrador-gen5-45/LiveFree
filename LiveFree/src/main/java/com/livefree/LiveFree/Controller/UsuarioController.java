45c4c1bf37a2f365d7ba6ed01f839b820960b7eb
Marco Alexandre <marcoalexandre554@gmail.com>
2022-03-11 13:21:56 -0300

    @MzAlexandre [TASK: deploy]

--- LiveFree/src/main/java/com/livefree/LiveFree/Controller/UsuarioController.java
+++ LiveFree/src/main/java/com/livefree/LiveFree/Controller/UsuarioController.java
@@ -1,18 +1,22 @@
 package com.livefree.LiveFree.Controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.livefree.LiveFree.Model.UserLogin;
 import com.livefree.LiveFree.Model.Usuario;
+import com.livefree.LiveFree.Repository.UsuarioRepository;
 import com.livefree.LiveFree.Service.UsuarioService;
 
 @RestController
@@ -23,6 +27,10 @@ public class UsuarioController {
 	
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@Autowired
+	private UsuarioRepository repository;
+
 
 	@SuppressWarnings("rawtypes")
 	@PostMapping("/logar")
@@ -35,6 +43,19 @@ public class UsuarioController {
 	public ResponseEntity post(@RequestBody Usuario usuario){
 		return usuarioService.CadastrarUsuario(usuario);
 	}
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<Usuario>> getAll() {
+
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> put(@RequestBody Usuario usuario){
+		return ResponseEntity.ok(repository.save(usuario));
+	}
+
+
 	
 	
 }
